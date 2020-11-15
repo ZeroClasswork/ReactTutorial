6e96c751309712cf94a5c0b1310bbe12e5f65d01
atleastzero <megan.obryan52@gmail.com>
2020-11-14 16:21:13 -0800

    Added content using props.

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function POPOSSpace() {
+function POPOSSpace(props) {
+  const  { name, image, address } = props
   return (
     <div>
       <img src={`${process.env.PUBLIC_URL}images/50-california-st.jpg`} width="300" height="300" alt="Hello" />
