b97d1c54c11ce8fe940d78257d7c45c2838d1321
atleastzero <megan.obryan52@gmail.com>
2020-11-23 14:56:10 -0800

    Added styling to POPOSSpace

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import './POPOSSpace.css'
+
 function POPOSSpace(props) {
   const  { name, image, address } = props
   return (
-    <div>
+    <div className="POPOSSpace">
       <img src={`${process.env.PUBLIC_URL}images/50-california-st.jpg`} width="300" height="300" alt="Hello" />
       <h1>Name...</h1>
       <div>Address...</div>
