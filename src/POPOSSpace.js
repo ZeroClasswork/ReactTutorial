750ab4e867887ff9ed5426e63869fc22300622fc
atleastzero <megan.obryan52@gmail.com>
2020-11-25 14:57:06 -0800

    Added Link to Spaces

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 import './POPOSSpace.css'
 
 function POPOSSpace(props) {
-  const  { name, image, address, hours } = props
+  const  { name, image, address, hours, id } = props
   return (
-    <div className="POPOSSpace">
+    <Link to={`/details/${id}`} className="POPOSSpace">
       <h1>{name}</h1>
       <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
       <div>{address}</div>
       <div>{hours}</div>
-    </div>
+    </Link>
   )
 }
 
